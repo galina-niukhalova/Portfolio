f24588c519605df1a642e57ffabe32ba2f965d86
Galina <galina.hvan1011@gmail.com>
2018-05-15 09:45:25 +0700

    animation

--- js/index.js
+++ js/index.js
@@ -16,3 +16,37 @@ $(document).ready(function () {
         }, offset: -$(".banner")[0].clientHeight/2
     });
 });
+
+
+    /* ------------------------------- */
+    /*  Animation on scroll  */
+    /* ------------------------------- */
+
+    var waypoint = new Waypoint({
+        element: $(".js--wp-1")[0],
+        handler: () => {
+            $(".js--wp-1").addClass('animated fadeInLeft');
+        }, offset: 'bottom-in-view'
+        // }, offset: $(".js--wp-1")[0].clientHeight
+    });
+
+    var waypoint = new Waypoint({
+        element: $(".js--wp-2")[0],
+        handler: () => {
+            $(".js--wp-2").addClass('animated fadeInRight');
+        }, offset: 'bottom-in-view'//, offset: $(".js--wp-2")[0].clientHeight
+    });
+
+    var waypoint = new Waypoint({
+        element: $(".js--wp-3")[0],
+        handler: () => {
+            $(".js--wp-3").addClass('animated fadeInLeft');
+        }, offset: $(".js--wp-3")[0].clientHeight
+    });
+
+    var waypoint = new Waypoint({
+        element: $(".js--wp-4")[0],
+        handler: () => {
+            $(".js--wp-4").addClass('animated fadeInRight');
+        }, offset: $(".js--wp-4")[0].clientHeight
+    });
